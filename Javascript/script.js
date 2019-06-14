142280a46a270b8701813788145788f2e866386f
Randall Barker <randall.jaren.barker@gmail.com>
2019-06-14 11:59:26 -0600

    6/14/2019

--- Javascript/script.js
+++ Javascript/script.js
@@ -14,7 +14,7 @@ ageVerification(55); // Can rent car
 
 // Recommended Ternary Operator (Sets Variable)
 function ageVerification2(age){
-  let answer = age > 25 ? "Can rent car" : "Not old enough";
+  let answer = age > 25 ? ("Can rent car" : "Not old enough");
   console.log(answer)
 }
 ageVerification2(5); // Not Old Enough
@@ -24,3 +24,41 @@ function ageVerification3(age){
   age > 25 ? console.log("Can rent car" : "Not old enough");
 }
 ageVerification3(30); // Can rent car
+
+
+// For Loop
+var array = ["Aye","Bee","See","Dee","Eee","Eff","Jee"];
+for(var i=0; i < array.length; i++){
+	console.log(array[i]); // Aye | Bee | See | Dee | Eee | Eff | Jee
+};
+
+// For Each
+var array = ["Aye","Bee","See","Dee","Eee","Eff","Jee"];
+array.forEach(function(element){
+	console.log(element); // Aye | Bee | See | Dee | Eee | Eff | Jee
+});
+
+// Looping through Objects
+var dic = {
+	hello: "Value1",
+	state: "Iowa",
+};
+for (var key in dic){
+	console.log(key + " => " + dic[key]); // hello => Value1 | state => Iowa
+}
+
+// While Loop (Conditional Check before initial Run)
+var arr2 = ["One","Two","Seven"];
+var i=0;
+while(i < arr2.length){
+  console.log(arr2[i]); // One | Two | Three
+  i++;
+}
+
+// Do Loop (Conditional Check after intitial Run)
+var arr2 = ["One","Two","Seven"];
+var i = 0;
+do{
+  console.log(arr2[i]); // One | Two | Three
+  i++;
+} while(i < arr2.length)
