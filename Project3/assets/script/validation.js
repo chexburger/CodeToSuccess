cd5c1461c3a5d0916b3faa9daed126d91cf1440f
Randall Barker <randall.jaren.barker@gmail.com>
2019-06-25 09:54:36 -0600

    a

--- Project3/assets/script/validation.js
+++ Project3/assets/script/validation.js
@@ -1,45 +1,48 @@
-$(function() {
-	$("form[name='validation']").validate({
-		//Validation rules
-		rules: {
-			//The key name is the name attribute of an input field.
-			//Define the validation rules for each field
-			firstname: "required",
-			lastname: "required",
-      message: "required",
-      subject: "required",
-			//If multiple rulls are required place in brackets
-			email: {
-				required: true,
-				//use the build in email rule
-				email: true
+$(document).ready(function(){
+	$(function() {
+		$("form[name='validation']").validate({
+			//Validation rules
+			rules: {
+				//The key name is the name attribute of an input field.
+				//Define the validation rules for each field
+				firstname: "required",
+				lastname: "required",
+	      message: "required",
+	      subject: "required",
+				//If multiple rulls are required place in brackets
+				email: {
+					required: true,
+					//use the build in email rule
+					email: true
+				},
+				password: {
+					required: true,
+					minlength: 5
+				},
+				password_confirm: {
+					equalTo: "#password"
+				}
 			},
-			password: {
-				required: true,
-				minlength: 5
-			},
-			password_confirm: {
-				equalTo: "#password"
-			}
-		},
 
-		//Setup error messages
-		messages: {
-			firstname: "Please enter your First Name",
-			lastname:  "Please enter your Last Name",
-      message: "Message content cannot be Empty",
-      subject: "A Message Subject is Required",
-			password: {
-				required: "Please provide a password",
-				minlength: "Password must be 5+ Characters"
+			//Setup error messages
+			messages: {
+				firstname: "Please enter your First Name",
+				lastname:  "Please enter your Last Name",
+	      message: "Message content cannot be Empty",
+	      subject: "A Message Subject is Required",
+				password: {
+					required: "Please provide a password",
+					minlength: "Password must be 5+ Characters"
+				},
+				password_confirm: {
+					equalTo: "Passwords must match"
+				},
+				email: "Please enter a valid email address"
 			},
-			password_confirm: {
-				equalTo: "Passwords must match"
-			},
-			email: "Please enter a valid email address"
-		},
-		submitHandler: function(form) {
-			form.submit();
-		}
+			submitHandler: function(form) {
+				form.submit();
+			}
+		});
 	});
+
 });
