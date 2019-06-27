623cd1308fecf5eaeffcc5e4b7bd783b8f2974fa
Randall Barker <randall.jaren.barker@gmail.com>
2019-06-27 11:22:01 -0600

    f

--- React/react-portfolio/src/components/portfolio/portfolio-container.js
+++ React/react-portfolio/src/components/portfolio/portfolio-container.js
@@ -7,21 +7,52 @@ export default class PortfolioContainer extends Component{
   constructor(){
     super();
 
-    console.log("Portfolio Container has rendered.")
+    this.state = {
+      pageTitle: "Welcome to my portfolio",
+      isLoading: false,
+      data: [
+        {title: "Google", category: "stuff", slug: "google"},
+        {title: "Microsoft", category: "more stuff", slug: "microsoft"},
+        {title: "Facebook", category: "also more stuff", slug: "facebook"},
+        {title: "AOL", category: "you've got mail", slug: "aol"}
+      ]
+    };
+    this.handleFilter = this.handleFilter.bind(this);
+    this.handlePageTitleUpdate = this.handlePageTitleUpdate.bind(this);
+  }
+
+  handleFilter(filter){
+    this.setState({
+      data: this.state.data.filter(item => {
+        return item.category === filter;
+      })
+    })
   }
 
   portfolioItems(){
-    const data = ["Google","Microsoft","Facebook","AOL"];
+    return this.state.data.map(item => {
+      return <PortfolioItem title={item.title} url={"google.com"} slug={item.slug} />;
+    })
+  }
 
-    return data.map(item => {
-      return <PortfolioItem title={item} url={"google.com"} />;
+  handlePageTitleUpdate() {
+    this.setState({
+      pageTitle: "Something Else"
     })
   }
 
   render(){
+    if(this.state.isLoading){
+      return <div>Loading...</div>
+    }
     return(
       <div>
-        <h2>Portfolio Items Here</h2>
+        <h2>{this.state.pageTitle}</h2>
+
+        <button onClick={() => this.handleFilter('stuff')}>stuff</button>
+        <button onClick={() => this.handleFilter('more stuff')}>more stuff</button>
+        <button onClick={() => this.handleFilter('also more stuff')}>also more stuff</button>
+        <button onClick={() => this.handleFilter("you've got mail")}>you've got mail</button>
 
         {this.portfolioItems()}
       </div>
